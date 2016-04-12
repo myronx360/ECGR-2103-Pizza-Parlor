008dea4285b5643cdb7900023ba8419b4d5f6788
myronx360 <mxw13@hotmail..com>
2016-04-12 11:37:18 -0400

    changed showTotal()

--- main.cpp
+++ main.cpp
@@ -50,35 +50,37 @@ const double LARGE_PRICE = 14.00;
 const double EXTRA_LARGE_PRICE = 14.00;
 const double NY_STYLE_PRICE = 16.00;
 
-
+double total = 0.00;
 
 
 
 void showMenu();
 
 int addPizza(string &pizzaName);
-void addCrust();
-void addToppings();
-void addGuests(int servingSize, string pizzaName);
+string addCrust();
+string addToppings();
+int addGuests(int servingSize, string pizzaName);
 
-void showTotal();
+void showTotal(string pizzaName, string crust, string toppings, int numberOfGuests);
 void showItemizedTotal();
 void showQOHTotals();
 
 int main() {
     string pizzaName = " ";
     int pizzaChoice = 0;
+    string crust;
+    string toppings;
+    int numberOfGuests = 0;
     for(int i = 0;i < 4;i++){
                 
         pizzaChoice = addPizza(pizzaName);
-        addCrust();
-        addToppings();
-        addGuests(pizzaChoice, pizzaName);
-        showTotal();
+        crust = addCrust();
+        toppings = addToppings();
+        numberOfGuests = addGuests(pizzaChoice, pizzaName);
+        showTotal(pizzaName, crust, toppings, numberOfGuests);
         
+        total = 0;
     }
-    
-
     return 0;
 }
 
@@ -92,32 +94,38 @@ int main() {
         
         switch(pizzaChoice){
             case 1: 
-                pizzaName = "Personal Pan";
+                pizzaName = "personal pan";
+                total+=PERSONAL_PAN_PRICE;
                 cout << "You have chosen to order a  Personal Pan pizza. This will feed " 
                 << PERSONAL_PAN_SIZE <<" people";
                 break;
             case 2: 
                 pizzaName = "small";
-                cout << "You have chosen to order a samll pizza. This will feed " 
+                total+=SMALL_PRICE;
+                cout << "You have chosen to order a smMll pizza. This will feed " 
                 << SMALL_SIZE <<" people";
                 break;
             case 3: 
                 pizzaName = "medium";
+                total+=MEDIUM_PRICE;
                 cout << "You have chosen to order a ______. This will feed " 
                 << MEDIUM_SIZE <<" people";
                 break;
             case 4: 
                 pizzaName = "";
+                total+=LARGE_PRICE;
                 cout << "You have chosen to order a _______. This will feed " 
                 << LARGE_SIZE <<" people";
                 break;
             case 5: 
                 pizzaName = "";
+                total+=EXTRA_LARGE_PRICE;
                 cout << "You have chosen to order a _______. This will feed " 
                 << EXTRA_LARGE_SIZE <<" people";
                 break;
             case 6: 
                 pizzaName = "";
+                total+=NY_STYLE_PRICE;
                 cout << "You have chosen to order a ______. This will feed " 
                 << NY_STYLE_SIZE <<" people";
                 break;
@@ -129,8 +137,9 @@ int main() {
         return pizzaChoice;
     }
     
-    void addCrust(){
+    string addCrust(){
         int crust = 0;
+        string crustName;
         cout << "Please enter a number from 1 to 3 to indicate the type of crust you want."
                 << endl;
         cin >> crust;
@@ -138,60 +147,83 @@ int main() {
         switch(crust){
             case 1:
                 cout << "You have chosen ______  as your crust" << endl;
+                crustName = "Thin Crust";
                 break;
             case 2:
                 cout << "You have chosen ______  as your crust" << endl;
+                crustName = "Flatbread Crust";
                 break;
                 cout << "You have chosen ______  as your crust" << endl;
             case 3:
                 cout << "You have chosen ______  as your crust" << endl;
+                crustName = "Thick Crust";
+                total+= 2.00;
                 break;
             default:
                 cout << "No crust.....OK" << endl;
+                crustName = "";
                 addCrust();
         }
+        return crustName;
     }
     
-    void addToppings(){
+    string addToppings(){
         int toppings = 0;
         char addMore = 'y';
+        string listOfToppings;
+    do {
+        
         cout << "Please enter a number from 1 to 10 to indicate what toppings you would like on your pizza" 
                 << endl;
         
         cin >> toppings;
         
-        do {
+      
 
             switch(toppings){
                 case 1:
                     cout << "You have added _____ to your order. Would you like to add more toppings (Y = Yes N = N)" << endl;
+                    listOfToppings+= "Cheese";
                     break;
                 case 2:
                     cout << "You have added _____ to your order. Would you like to add more toppings (Y = Yes N = N)" << endl;
+                    listOfToppings+= "Pepperoni";
                     break;
                 case 3:
                     cout << "You have added _____ to your order. Would you like to add more toppings (Y = Yes N = N)" << endl;
+                    listOfToppings+= "Mushroom";
+                    total+=1.00;
                     break;
                 case 4:
                     cout << "You have added _____ to your order. Would you like to add more toppings (Y = Yes N = N)" << endl;
+                    listOfToppings+= "Onions";
+                    total+=1.00;
                     break;
                 case 5:
                     cout << "You have added _____ to your order. Would you like to add more toppings (Y = Yes N = N)" << endl;
+                    listOfToppings+= "";
                     break;
                 case 6:
                     cout << "You have added _____ to your order. Would you like to add more toppings (Y = Yes N = N)" << endl;
+                    listOfToppings+= "";
                     break;
                 case 7:
                     cout << "You have added _____ to your order. Would you like to add more toppings (Y = Yes N = N)" << endl;
+                    listOfToppings+= "";
+                    total+=1.00;
                     break;
                 case 8:
                     cout << "You have added _____ to your order. Would you like to add more toppings (Y = Yes N = N)" << endl;
+                    listOfToppings+= "";
                     break;
                 case 9:
                     cout << "You have added _____ to your order. Would you like to add more toppings (Y = Yes N = N)" << endl;
+                    listOfToppings+= "";
                     break;
                 case 10:
                     cout << "You have added _____ to your order. Would you like to add more toppings (Y = Yes N = N)" << endl;
+                    listOfToppings+= "";
+                    total+=1.00;
                     break;
 
                 default:
@@ -201,11 +233,13 @@ int main() {
             
             cin >> addMore;
         
-        } while(addMore == 'y');        
+        } while(addMore == 'y');    
+        return listOfToppings;
     }
 
-    void addGuests(int numberOfSlices, string pizzaName){
+    int addGuests(int numberOfSlices, string pizzaName){
         int guests = 0;
+        
         do {
             cout << "Please enter the number of guests you have in your party" << endl;
 
@@ -242,9 +276,19 @@ int main() {
             }
             
         }while(guests > numberOfSlices);
+        return guests;
     }
     
-    void showTotal(){
-     
-        //showQOHTotals()
+    void showTotal(string pizzaName, string crust, string toppings, int numberOfGuests){
+        char showItems;
+        cout << "Great your total for today’s order at the Pizza Parlor was $" << total << endl;
+        cout << "Would you like an itemized list of your order? (Y = Yes N = No)" << endl;
+        cin >> showItems;
+        
+        if(showItems == 'y'){
+            //showItemizedTotal();
+        }
+        
+         cout << "The total cost for each of your " <<  numberOfGuests << "guests will be "
+                 << "$"<< (total / numberOfGuests);           
     }
